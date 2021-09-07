bcb65143d918d08702be48fb6c92a1dae6b981e5
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2021-09-07 17:27:48 -0600

    print raw

--- lab1.py
+++ lab1.py
@@ -4,3 +4,8 @@ print("Requests version:", requests.__version__)
 r = requests.get('https://www.google.com/')
 print(r) # print status
 print(r.content) # print content
+print()
+
+print('Source code from github:')
+r = requests.get('https://raw.githubusercontent.com/tylerelston/cmput404-lab1/main/lab1.py')
+print(r.content)
